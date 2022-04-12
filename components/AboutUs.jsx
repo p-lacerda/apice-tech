acce7f421f6bdb5a5041a0f25de078f29e36616d
p-lacerda <xcloudgameemail@gmail.com>
2022-04-11 21:07:20 -0300

    update text

--- components/AboutUs.jsx
+++ components/AboutUs.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 function AboutUs() {
+  const englishText = <p>We at Odessa are a freelancer group on both mobile and web development. Our main objetive is to bring the best in the technology sector, with creativity, modernism and innovation. Our vision is to grow more and more and become a reference in one of the fastest growings professional sectors around the world.</p>
+  const englishTitle = 'About Us';
+
   return (
     <div className='text-white'>
       <h3 className='text-2xl font-bold mb-4'>Sobre nós</h3>
-      <p>Nós da Odessa somos uma empresa freelancer focada em desenvolvimento tanto mobile quanto web. Nosso principal objetivo é trazer o que há de melhor no setor de tecnologia, com criatividade, praticidade e inovação. Nossa ambição é crescer cada vez mais e nos tornarmos referência </p>
+      <p>Nós da Odessa somos um grupo freelancer focada em desenvolvimento tanto mobile quanto web. Nosso principal objetivo é trazer o que há de melhor no setor de tecnologia, com criatividade, excelência e inovação. Nossa visão é crescer cada vez mais e nos tornarmos referência num dos setores profissionais que mais crescem ao redor do mundo. </p>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='h-100 bg-stone-900'>
-        <Navbar />
+      <Navbar />
+      <main className='h-100 bg-stone-900 sm:max-w-screen-sm md:max-w-screen-xl m-auto'>
         <div className='px-5 space-y-6'>
           <TopContent />
           <AboutUs />
